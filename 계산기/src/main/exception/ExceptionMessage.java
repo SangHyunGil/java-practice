a421e63abef02e926460e14f24778409276aaedf
SangHyunGil <zizon5941@gmail.com>
2021-12-18 10:53:31 +0900

    feat: exception 처리 구현(문자열 계산기)

--- 계산기/src/main/exception/ExceptionMessage.java
+++ 계산기/src/main/exception/ExceptionMessage.java
@@ -4,5 +4,6 @@ public class ExceptionMessage {
     public static final String EXPRESSION_BLANK = "입력이 공백입니다.";
     public static final String DIVIDE_BY_ZERO = "분모는 0이 될 수 없습니다.";
     public static final String HAS_NO_PROPER_OPERATOR = "적절한 연산자가 존재하지 않습니다.";
-
+    public static final String SIZE_EVEN = "연산 문자열의 길이는 짝수가 될 수 없습니다.";
+    public static final String SIZE_DOWN_3 = "사이즈는 3이하가 될 수 없습니다.";
 }
--- 계산기/src/main/util/StringUtil.java
+++ 계산기/src/main/util/StringUtil.java
@@ -10,6 +10,8 @@ public class StringUtil {
     public static String[] split(String input) {
         validateInput(input);
         String[] result = input.split(" ");
+        validateSizeDown3(result);
+        validateSizeEven(result);
         return result;
     }
 
@@ -20,5 +22,15 @@ public class StringUtil {
 
     private static boolean hasNoInput(String input) {
         return input == null || input.trim().isEmpty();
+    }
+
+    private static void validateSizeDown3(String[] result) {
+        if (result.length < 3)
+            throw new IllegalArgumentException(ExceptionMessage.SIZE_DOWN_3);
+    }
+
+    private static void validateSizeEven(String[] result) {
+        if (result.length % 2 == 0)
+            throw new IllegalArgumentException(ExceptionMessage.SIZE_EVEN);
     }
 }
